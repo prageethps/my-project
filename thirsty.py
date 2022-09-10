2e7c25eca04b3516af26dd2d9b97270e3759ddde
prageethps <113235761+prageethps@users.noreply.github.com>
2022-09-10 22:42:43 +0530

    Create thirsty.py

---
+++ thirsty.py
@@ -0,0 +1,10 @@
+hungry= input("are you hungry?")
+if hungry=="yes":
+    print("eat rice")
+    print("eat pizza")
+    print("eat burger")
+else:
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+        print("drink water")
+        print("drink fanta")
